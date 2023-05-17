940d4659b04365a690b5892c28ebaef55ff41529
Gunjan Kumari <112612648+gunjan6@users.noreply.github.com>
2023-05-17 07:39:59 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,21 @@
+// For loop 
+
+for (let i = 0; i<=5; i++){
+  console.log(i);
+}
+
+// While loop 
+
+let i =0;
+while (i<10){
+  console.log(i);
+  i++;
+}
+
+// Do while loop 
+
+let i = 0; 
+do {
+  console.log(i);
+  i++;
+} while(i<10);
