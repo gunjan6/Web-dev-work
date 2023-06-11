1e699207df81f4ce9d0816748702a45c4d80d87b
Gunjan Kumari <112612648+gunjan6@users.noreply.github.com>
2023-06-11 22:04:03 +0530

    math-function.js

--- math-function.js
+++ math-function.js
@@ -1,4 +1,4 @@
-math.random(); // to generate a random number 
-math.round(); // to round a number 
-math.max(); // to find a maximum number 
-math.min(); // to find a minimum number 
+math.random(); 
+math.round();
+math.max(); 
+math.min(); 
